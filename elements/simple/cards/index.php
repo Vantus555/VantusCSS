d18bbb99db128036c358d8a4b8a9bcdb7da41d5a
Vantus555 <nindzay1998@gmail.com>
2020-09-10 10:13:42 +0300

    display. borders. outlines. margins. puddings. text.

---
+++ elements/simple/cards/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../mainStyle.css">
+    <style>
+        <?php require_once '../../../vcss_fullpack.php'; ?>
+    </style>
+</head>
+<body>
+    <a class="back" href="../index.php">Назад</a>
+    <div class="list">
+        <div class="listitemelem">
+            <div class="element">
+                <button class="v-btn v-bg-focus v-bg-primary v-btn-bootstrap">Кнопка</button>
+                <button class="v-btn v-bg-focus v-bg-secondary v-btn-bootstrap">Кнопка</button>
+                <button class="v-btn v-bg-focus v-bg-success v-btn-bootstrap">Кнопка</button>
+                <button class="v-btn v-bg-focus v-bg-danger v-btn-bootstrap">Кнопка</button>
+                <button class="v-btn v-bg-focus v-bg-pink v-btn-bootstrap">Кнопка</button>
+            </div>
+            <code class="code-element">
+                <div class="listitemelem">
+                    &lt;button class="v-btn v-bg-focus v-bg-primary v-btn-bootstrap"&gt;Кнопка&lt;/button&gt;
+                </div>
+                <div class="listitemelem">
+                    &lt;button class="v-btn v-bg-focus v-bg-secondary v-btn-bootstrap"&gt;Кнопка&lt;/button&gt;
+                </div>
+                <div class="listitemelem">
+                    &lt;button class="v-btn v-bg-focus v-bg-success v-btn-bootstrap"&gt;Кнопка&lt;/button&gt;
+                </div>
+                <div class="listitemelem">
+                    &lt;button class="v-btn v-bg-focus v-bg-primary v-btn-bootstrap"&gt;Кнопка&lt;/button&gt;
+                </div>    
+                <div class="listitemelem">
+                    &lt;button class="v-btn v-bg-focus v-bg-pink v-btn-bootstrap"&gt;Кнопка&lt;/button&gt;
+                </div>              
+            </code>
+        </div>
+    </div>
+</body>
+</html>
--- vcss_fullpack.php
+++ vcss_fullpack.php
@@ -1,8 +1,15 @@
 <?php
     require_once 'elements/simple/buttons/btn.css';
     require_once 'elements/simple/buttons/bootstrap.css';
-    require_once 'elements/simple/buttons/shadow.css';
-    require_once 'elements/simple/buttons/shadow-move.css';
-    require_once 'elements/simple/colors/background.css';
-    require_once 'elements/simple/colors/colors.css';
+    require_once 'elements/properties/shadow.css';
+    require_once 'elements/properties/shadow-move.css';
+    require_once 'elements/properties/colors/background.css';
+    require_once 'elements/properties/colors/colors.css';
+    require_once 'elements/properties/paddings&margin/pm.css';
+    require_once 'elements/properties/text/text.css';
+    require_once 'elements/properties/display/display.css';
+    require_once 'elements/properties/shadow/shadow.css';
+    require_once 'elements/properties/shadow/shadow-move.css';
+    require_once 'elements/properties/w&h/w&h.css';
+    require_once 'elements/properties/borders&outlines/bo.css';
 ?>
