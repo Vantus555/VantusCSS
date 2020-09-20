3e6dbcf16167e13751360d7ba43011da4940cd94
Vantus555 <nindzay1998@gmail.com>
2020-09-20 12:43:00 +0300

    media display grid/ cards group 2

--- elements/medium/card-groups/index.php
+++ elements/medium/card-groups/index.php
@@ -14,8 +14,8 @@
     <div class="list v-container-1440">
         <div class="listitemelem">
             <div class="element v-container-full v-bg-light "> 
-                <div class="v-container-1440 v-d-flex">
-                    <div class="v-flex-center v-flex-col-3">
+                <div class="v-container-1440 v-grid-col-3">
+                    <div class="v-flex-center ">
                         <div class="v-vcard-default v-bg-light v-container-360">
                             <img src="imgs/720x400.png" alt="">
                             <div>
@@ -30,7 +30,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="v-flex-center v-flex-col-3">
+                    <div class="v-flex-center ">
                         <div class="v-vcard-default v-bg-light v-container-360">
                             <img src="imgs/720x400.png" alt="">
                             <div>
@@ -45,7 +45,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="v-flex-center v-flex-col-3">
+                    <div class="v-flex-center ">
                         <div class="v-vcard-default v-bg-light v-container-360">
                             <img src="imgs/720x400.png" alt="">
                             <div>
@@ -56,6 +56,141 @@
                                     microdosing tousled waistcoat.</p>
                                 <div>
                                     <a href="#" class="more">Link more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="v-flex-center">
+                        <div class="v-vcard-default v-bg-light v-container-360">
+                            <img src="imgs/720x400.png" alt="">
+                            <div>
+                                <p>Category</p>
+                                <p>The catalyzer</p>
+                                <p>
+                                    Photo booth fam kinfolk cold-pressed sriracha leggings jianbing 
+                                    microdosing tousled waistcoat.</p>
+                                <div>
+                                    <a href="#" class="more">Link more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="listitemelem">
+            <div class="element v-container-full v-bg-light "> 
+                <div class="v-container-1440 v-grid-col-3">
+                    <div class="v-flex-center ">
+                        <div class="v-card-fastfood-wrap v-bg-dark v-container-360">
+                            <div></div>
+                            <div class="v-card-fastfood">
+                                <h3>McDonald`s</h3>
+                                <img src="imgs/fastfood.png" alt="">
+                                <span>$9.99</span>
+                                <h1>Hamburger</h1>
+                                <p>Тот самый Бургер с двумя большими рублеными 
+                                    бифштексами из 100% говядины на булочке с кунжутом. 
+                                    Особенный вкус сандвичу придают соус «Гриль» с дымком, 
+                                    3 кусочка сыра «эмменталь», ломтик помидора, свежий салат 
+                                    и лук.
+                                </p>
+                                <hr>
+                                <div class="v-fastfood-calories">
+                                    <span>Calories</span>
+                                    <span>229 kcal</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Carbohydrate</span>
+                                    <span>19 gr</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Protein</span>
+                                    <span>13 gr</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Fat</span>
+                                    <span>12 gr</span>
+                                </div>
+                                <hr>
+                                <div class="v-buy">
+                                    <button class="v-btn v-radius-5 v-bg-active v-bg-yellow v-shadow v-shadow-active">В корзину</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="v-flex-center ">
+                        <div class="v-card-fastfood-wrap v-bg-dark v-container-360">
+                            <div></div>
+                            <div class="v-card-fastfood">
+                                <h3>McDonald`s</h3>
+                                <img src="imgs/fastfood.png" alt="">
+                                <span>$9.99</span>
+                                <h1>Hamburger</h1>
+                                <p>Тот самый Бургер с двумя большими рублеными 
+                                    бифштексами из 100% говядины на булочке с кунжутом. 
+                                    Особенный вкус сандвичу придают соус «Гриль» с дымком, 
+                                    3 кусочка сыра «эмменталь», ломтик помидора, свежий салат 
+                                    и лук.
+                                </p>
+                                <hr>
+                                <div class="v-fastfood-calories">
+                                    <span>Calories</span>
+                                    <span>229 kcal</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Carbohydrate</span>
+                                    <span>19 gr</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Protein</span>
+                                    <span>13 gr</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Fat</span>
+                                    <span>12 gr</span>
+                                </div>
+                                <hr>
+                                <div class="v-buy">
+                                    <button class="v-btn v-radius-5 v-bg-active v-bg-yellow v-shadow v-shadow-active">В корзину</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="v-flex-center ">
+                        <div class="v-card-fastfood-wrap v-bg-dark v-container-360">
+                            <div></div>
+                            <div class="v-card-fastfood">
+                                <h3>McDonald`s</h3>
+                                <img src="imgs/fastfood.png" alt="">
+                                <span>$9.99</span>
+                                <h1>Hamburger</h1>
+                                <p>Тот самый Бургер с двумя большими рублеными 
+                                    бифштексами из 100% говядины на булочке с кунжутом. 
+                                    Особенный вкус сандвичу придают соус «Гриль» с дымком, 
+                                    3 кусочка сыра «эмменталь», ломтик помидора, свежий салат 
+                                    и лук.
+                                </p>
+                                <hr>
+                                <div class="v-fastfood-calories">
+                                    <span>Calories</span>
+                                    <span>229 kcal</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Carbohydrate</span>
+                                    <span>19 gr</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Protein</span>
+                                    <span>13 gr</span>
+                                </div>
+                                <div class="v-fastfood-calories">
+                                    <span>Fat</span>
+                                    <span>12 gr</span>
+                                </div>
+                                <hr>
+                                <div class="v-buy">
+                                    <button class="v-btn v-radius-5 v-bg-active v-bg-yellow v-shadow v-shadow-active">В корзину</button>
                                 </div>
                             </div>
                         </div>
